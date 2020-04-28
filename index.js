550a0c77fb6da3c16b3ddb7291e814db0a2fd517
TomDev24 <mail.com67747@gmail.com>
2020-04-28 13:26:33 +0300

    Read functianality is done. Added pug and fs module to read file

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
-app.use(express.static('public'));
+app.set('view engine', 'pug');
+//app.use(express.static('public'));
 
 app.get('/', function(req, res){
-	res.send('Hello world!');
+	arrayOfHospitalsObject =getDataFromIpu();
+	res.render('index', {message: 'Hello there!', arrayOfHospitalsObject});
 });
 
 app.listen(3000, function(){
 	console.log('Example app is listening!');
 });
+
+function getDataFromIpu(){
+	var jsonObj = JSON.parse(fs.readFileSync('ipu.json', 'utf8'));
+	arrayOfHospitalsObject = [];
+	for (idObj in jsonObj){
+		arrayOfHospitalsObject.push(jsonObj[idObj]);
+	}
+	return arrayOfHospitalsObject;
+}
